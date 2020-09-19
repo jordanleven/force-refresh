768e7334743c6273c73d0e99f80b98a81e040e64
Jordan Leven <jordanleven@me.com>
2020-09-19 13:31:30 -0400

    test: Add test for client API file

---
+++ includes/api/client/client.Test.php
@@ -0,0 +1,11 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/client.php';
+
+class Client extends TestCase {
+
+    public function testAssertFileImports() {
+        $this->assertTrue( true );
+    }
+}
