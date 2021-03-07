86dc5a97f813cd88f2046aa2e3d266ece6cdd43b
Jordan Leven <jordanleven@me.com>
2021-03-07 11:35:49 -0500

    chore: Update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,6 @@
 PRODUCTION_BRANCH="master"
 MAIN_FILE="filter-force-refresh.php"
 CURRENT_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-CURRENT_PACKAGE_VERSION=$(node -e "process.stdout.write(require('./package.json').version)")
-NEXT_VERSION=$(npx next-standard-version)
 
 if [[ $CURRENT_BRANCH != $PRODUCTION_BRANCH ]]
   then
@@ -10,6 +8,23 @@ if [[ $CURRENT_BRANCH != $PRODUCTION_BRANCH ]]
   exit 1
 fi
 
+RELEASE_OPTION=$1
+case $RELEASE_OPTION in
+  --major)
+    RELEASE_TYPE='major'
+    ;;
+  --minor)
+    RELEASE_TYPE='minor'
+    ;;
+  --patch)
+    RELEASE_TYPE='patch'
+    ;;
+esac
+
+# If we've specified a type of release, we need to include the `releaseAs` argument
+SPECIFIED_RELEASE_TYPE_ARG=$([ $RELEASE_TYPE ] && echo "--releaseAs $RELEASE_TYPE");
+CURRENT_PACKAGE_VERSION=$(node -e "process.stdout.write(require('./package.json').version)")
+NEXT_VERSION=$(npx next-standard-version ${SPECIFIED_RELEASE_TYPE_ARG})
 echo "\033[33mBump plugin from ${CURRENT_PACKAGE_VERSION} to ${NEXT_VERSION}?\033[0m\n"
 echo "\033[33mPress \"y\" to proceed with this release or press any other key to abort.\033[0m\n"
 read -p "" -n 1 -s
@@ -17,7 +32,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
   perl -pi -e "s/(?<=Version: ).*/${NEXT_VERSION}/g" $MAIN_FILE
   git add $MAIN_FILE
-  npx standard-version -a
+  npx standard-version -a $([ $RELEASE_TYPE ] && echo "--release-as $RELEASE_TYPE")
   git push --follow-tags
   echo "\033[1;32mPackage succesfully updated to ${NEXT_VERSION} and pushed to the remote.\033[0m\n"
 else
