b4723a1ae504e40ea39a204b709aaae0ad9d45c6
Jordan Leven <jordan@heysparkbox.com>
2021-11-29 13:50:35 -0500

    chore(release): 2.3.1-fix--issue-with-continual-refresh.0

--- filter-force-refresh.php
+++ filter-force-refresh.php
@@ -13,7 +13,7 @@ namespace JordanLeven\Plugins\ForceRefresh;
 Plugin Name: Force Refresh
 Plugin URI:
 Description: Force Refresh is a simple plugin that allows you to force a page refresh for users currently visiting your site.
-Version: 2.3.1-fix.1
+Version: 2.3.1-fix--issue-with-continual-refresh.
 Author: Jordan Leven
 Author URI: https://github.com/jordanleven
 Contributors:
