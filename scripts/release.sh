22d2106327026a0b15c37745907df7819fed9cea
Jordan Leven <jordanleven@me.com>
2021-12-04 15:44:31 -0500

    chore: Release script updates

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,40 +1,49 @@
 PRODUCTION_BRANCH="master"
-MAIN_FILE="filter-force-refresh.php"
-CURRENT_BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+function getReleaseType {
+  RELEASE_OPTION=$1
+  case $RELEASE_OPTION in
+    --major)
+      RELEASE_TYPE='major'
+      ;;
+    --minor)
+      RELEASE_TYPE='minor'
+      ;;
+    --patch)
+      RELEASE_TYPE='patch'
+      ;;
+  esac
+  echo $RELEASE_TYPE
+}
+
+function bumpVersionPackage {
+  RELEASE_TYPE=$1
+  SPECIFIED_RELEASE_TYPE_ARG=$([ ${RELEASE_TYPE} ] && echo "--releaseAs ${RELEASE_TYPE}")
+  npx standard-version -a ${SPECIFIED_RELEASE_TYPE_ARG}
+}
+
+CURRENT_BRANCH=$(getCurrentBranchName)
+RELEASE_TYPE=$(getReleaseType $1)
+# If we've specified a type of release, we need to include the `releaseAs` argument
+SPECIFIED_RELEASE_TYPE_ARG=$([ $RELEASE_TYPE ] && echo "--releaseAs ${RELEASE_TYPE}")
+NEXT_VERSION=$(npx standard-version --dry-run --skip.changelog --skip.commit --skip.tag ${SPECIFIED_RELEASE_TYPE_ARG})
 
 if [[ $CURRENT_BRANCH != $PRODUCTION_BRANCH ]]
   then
-  echo "\033[1;31mCannot release while on branch $CURRENT_BRANCH\033[0m"
+  echo "\033[1;31mCannot release while on branch ${CURRENT_BRANCH}\033[0m"
   exit 1
 fi
 
-RELEASE_OPTION=$1
-case $RELEASE_OPTION in
-  --major)
-    RELEASE_TYPE='major'
-    ;;
-  --minor)
-    RELEASE_TYPE='minor'
-    ;;
-  --patch)
-    RELEASE_TYPE='patch'
-    ;;
-esac
-
-# If we've specified a type of release, we need to include the `releaseAs` argument
-SPECIFIED_RELEASE_TYPE_ARG=$([ $RELEASE_TYPE ] && echo "--releaseAs $RELEASE_TYPE");
-CURRENT_PACKAGE_VERSION=$(node -e "process.stdout.write(require('./package.json').version)")
-NEXT_VERSION=$(npx next-standard-version ${SPECIFIED_RELEASE_TYPE_ARG})
-echo "\033[33mBump plugin from ${CURRENT_PACKAGE_VERSION} to ${NEXT_VERSION}?\033[0m\n"
+echo "\033[37m=====================\033[0m\n"
+echo "\033[37m${NEXT_VERSION}\033[0m\n"
 echo "\033[33mPress \"y\" to proceed with this release or press any other key to abort.\033[0m\n"
+
 read -p "" -n 1 -s
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  perl -pi -e "s/(?<=Version: ).*/${NEXT_VERSION}/g" $MAIN_FILE
-  git add $MAIN_FILE
-  npx standard-version -a $([ $RELEASE_TYPE ] && echo "--release-as $RELEASE_TYPE")
+  bumpVersionPackage $RELEASE_TYPE
   git push --follow-tags
-  echo "\033[1;32mPackage succesfully updated to ${NEXT_VERSION} and pushed to the remote.\033[0m\n"
+  echo "\033[1;32mPackage succesfully updated and pushed to the remote.\033[0m\n"
 else
   echo "\033[1;31mRelease was aborted.\033[0m\n"
 fi
