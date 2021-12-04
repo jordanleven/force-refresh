ec900b2b404ea4584a2603e126fd1046ea0a5418
Jordan Leven <jordanleven@me.com>
2021-12-04 15:44:03 -0500

    chore: Update release scripts
    Next standard version has a number of security issues and hasn't been
    updated in two years. Instead, we'll only use Standard Version.

--- scripts/releaseBeta.sh
+++ scripts/releaseBeta.sh
@@ -7,8 +7,8 @@ function getCurrentBranchName {
 
 function getPrereleaseName {
   CURRENT_BRANCH_NAME=$(getCurrentBranchName)
-  COMMIT_SHA=$(git log master..$CURRENT_BRANCH_NAME --oneline --format=format:%H | tail -1)
-  PRERELEASE_NAME=$(echo $COMMIT_SHA | cut -c1-6 | awk '{ print toupper($0) }')
+  COMMIT_SHA=$(git log master..${CURRENT_BRANCH_NAME} --oneline --format=format:%H | tail -1)
+  PRERELEASE_NAME=$(echo ${COMMIT_SHA} | cut -c1-6 | awk '{ print toupper($0) }')
   echo $PRERELEASE_NAME
 }
 
@@ -18,20 +18,20 @@ function getCurrentPackageVersion {
 
 function getExistingBuildNumber {
   CURRENT_PACKAGE_VERSION=$(getCurrentPackageVersion)
-  if [[ "$CURRENT_PACKAGE_VERSION" == *"-"* ]]; then
+  if [[ "${CURRENT_PACKAGE_VERSION}" == *"-"* ]]; then
       PRERELEASE_NAME=$(getPrereleaseName)
-      BUILD_VERSION=$(echo $CURRENT_PACKAGE_VERSION | sed 's/.*-'${PRERELEASE_NAME}'.//')
+      BUILD_VERSION=$(echo ${CURRENT_PACKAGE_VERSION} | sed 's/.*-'${PRERELEASE_NAME}'.//')
       echo $BUILD_VERSION
   fi
 }
 
 function getNextBuildNumber {
   EXISTING_BUILD_NUMBER=$(getExistingBuildNumber)
-  if [ -z "$EXISTING_BUILD_NUMBER" ]
+  if [ -z "${EXISTING_BUILD_NUMBER}" ]
     then
       echo 0
     else
-      echo "$(($EXISTING_BUILD_NUMBER + 1))"
+      echo "$((${EXISTING_BUILD_NUMBER} + 1))"
   fi
 }
 
@@ -42,50 +42,58 @@ function getNextVersion {
       echo "$(npx next-standard-version $1 $2)"
     else
       CURRENT_PACKAGE_VERSION=$(getCurrentPackageVersion)
-      PACKAGE_VERSION=(`echo $CURRENT_PACKAGE_VERSION | tr '-' ' '`)
+      PACKAGE_VERSION=(`echo ${CURRENT_PACKAGE_VERSION} | tr '-' ' '`)
       EXISTING_PACKAGE_VERSION=${PACKAGE_VERSION[0]}
       echo "${EXISTING_PACKAGE_VERSION}"
   fi
 }
 
-RELEASE_OPTION=$1
-case $RELEASE_OPTION in
-  --major)
-    RELEASE_TYPE='major'
-    ;;
-  --minor)
-    RELEASE_TYPE='minor'
-    ;;
-  --patch)
-    RELEASE_TYPE='patch'
-    ;;
-esac
+function getReleaseType {
+  RELEASE_OPTION=$1
+  case $RELEASE_OPTION in
+    --major)
+      RELEASE_TYPE='major'
+      ;;
+    --minor)
+      RELEASE_TYPE='minor'
+      ;;
+    --patch)
+      RELEASE_TYPE='patch'
+      ;;
+  esac
+  echo $RELEASE_TYPE;
+}
 
-# If we've specified a type of release, we need to include the `releaseAs` argument
-SPECIFIED_RELEASE_TYPE_ARG=$([ $RELEASE_TYPE ] && echo "--releaseAs $RELEASE_TYPE");
+function bumpVersionPackage {
+  RELEASE_TYPE=$1
+  PRERELEASE_NAME=$2
+  SPECIFIED_RELEASE_TYPE_ARG=$([ ${RELEASE_TYPE} ] && echo "--releaseAs ${RELEASE_TYPE}")
+  npx standard-version -a --prerelease ${PRERELEASE_NAME} --skip.changelog ${SPECIFIED_RELEASE_TYPE_ARG}
+}
+
 CURRENT_BRANCH=$(getCurrentBranchName)
-CURRENT_PACKAGE_VERSION=$(getCurrentPackageVersion)
-NEXT_VERSION=$(getNextVersion $SPECIFIED_RELEASE_TYPE_ARG)
+RELEASE_TYPE=$(getReleaseType $1)
+# If we've specified a type of release, we need to include the `releaseAs` argument
+SPECIFIED_RELEASE_TYPE_ARG=$([ ${RELEASE_TYPE} ] && echo "--releaseAs ${RELEASE_TYPE}")
 PRERELEASE_NAME=$(getPrereleaseName)
-NEXT_BUILD=$(getNextBuildNumber)
-NEXT_VERSION_AND_BUILD="${NEXT_VERSION}-${PRERELEASE_NAME}.${NEXT_BUILD}"
+NEXT_VERSION=$(npx standard-version --prerelease ${PRERELEASE_NAME} --dry-run --skip.changelog --skip.commit --skip.tag ${SPECIFIED_RELEASE_TYPE_ARG})
 
 if [[ $CURRENT_BRANCH == $PRODUCTION_BRANCH ]]
   then
-  echo "\033[1;31mCannot release betas while on branch $PRODUCTION_BRANCH\033[0m"
+  echo "\033[1;31mCannot release betas while on branch ${PRODUCTION_BRANCH}\033[0m"
   exit 1
 fi
 
-echo "\033[33mBump plugin from ${CURRENT_PACKAGE_VERSION} to ${NEXT_VERSION_AND_BUILD}?\033[0m\n"
-echo "\033[33mPress \"y\" to proceed with this release or press any other key to abort.\033[0m\n"
+echo "\033[37m=====================\033[0m\n"
+echo "\033[37m${NEXT_VERSION}\033[0m\n"
+echo "\033[33mPress \"y\" to proceed with this beta release or press any other key to abort.\033[0m\n"
+
 read -p "" -n 1 -s
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  perl -pi -e "s/(?<=Version: ).*/${NEXT_VERSION_AND_BUILD}/g" $MAIN_FILE
-  git add $MAIN_FILE
-  npx standard-version -a --prerelease $PRERELEASE_NAME --skip.changelog $([ $RELEASE_TYPE ] && echo "--release-as $RELEASE_TYPE")
+  bumpVersionPackage $RELEASE_TYPE $PRERELEASE_NAME
   git push --follow-tags
-  echo "\033[1;32mPackage succesfully updated to ${NEXT_VERSION_AND_BUILD} and pushed to the remote.\033[0m\n"
+  echo "\033[1;32mPackage succesfully updated and pushed to the remote.\033[0m\n"
 else
   echo "\033[1;31mRelease was aborted.\033[0m\n"
 fi
