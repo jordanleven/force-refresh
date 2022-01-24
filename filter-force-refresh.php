20c8ffd447cb3bcc51c6cdcb70a9ac6554ab3097
Jordan Leven <jordanleven@me.com>
2022-01-23 21:19:52 -0500

    chore: Update minimum WordPress Version
    Realistically, WordPress 5.2 will still work fine with Force Refresh.

--- filter-force-refresh.php
+++ filter-force-refresh.php
@@ -14,7 +14,7 @@ Plugin Name: Force Refresh
 Plugin URI: https://github.com/jordanleven/force-refresh
 Description: Force Refresh is a simple plugin that allows you to force a page refresh for users currently visiting your site.
 Version: 2.5.2
-Requires at least: 5.8
+Requires at least: 5.2
 Tested up to: 5.9
 Requires PHP: 7.4
 Author: Jordan Leven
