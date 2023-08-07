a5901dedbd09cb59a30c4e0cbe437378f26c2bd6
Jordan Leven <me@jordanleven.com>
2023-08-07 19:34:55 -0400

    style: Shell script linting fixes

--- .github/scripts/bumpWordPressPluginVersion.sh
+++ .github/scripts/bumpWordPressPluginVersion.sh
@@ -1,12 +1,13 @@
+#!/bin/sh
 MAIN_FILE="filter-force-refresh.php"
 
-function getCurrentPackageVersion {
-  echo $(node -e "process.stdout.write(require('./package.json').version)")
+getCurrentPackageVersion() {
+ node -e "process.stdout.write(require('./package.json').version)"
 }
 
-function bumpVersionPlugin {
-  PACKAGE_VERSION=$(getCurrentPackageVersion)
-  perl -pi -e "s/(?<=Version: ).*/${PACKAGE_VERSION}/g" ${MAIN_FILE}
+bumpVersionPlugin() {
+  package_version=$(getCurrentPackageVersion)
+  perl -pi -e "s/(?<=Version: ).*/${package_version}/g" ${MAIN_FILE}
   git add $MAIN_FILE
 }
 
